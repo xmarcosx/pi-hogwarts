b2262fa4c7c5335c788f2e04519f6fc95673d57f
Marcos Alcozer <marcos@alcozer.dev>
2021-11-26 20:06:46 -0600

    Removes loop

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from gpiozero import LED, MotionSensor
 pir = MotionSensor(4)
 led = LED(17)
 
-while True:
-	pir.wait_for_motion()
-	print("You moved")
-	led.on()
-	pir.wait_for_no_motion()
-	led.off()
+pir.wait_for_motion()
+print("You moved")
+led.on()
+pir.wait_for_no_motion()
+led.off()
